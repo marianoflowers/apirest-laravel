c13dfaaaacce4f2719f9287f9d3c65fa2d48ba89
marianoflowers <mariano.alfredo.flores@gmail.com>
2021-09-22 21:42:23 -0300

    [Create] Artículo Factory

---
+++ database/factories/ArticuloFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Articulo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticuloFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Articulo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'descripcion' => $this->faker->name(),
+            'stock' => $this->faker->numberBetween(10,30),
+            'precio' => $this->faker->numberBetween(20000,45000)
+        ];
+    }
+}
