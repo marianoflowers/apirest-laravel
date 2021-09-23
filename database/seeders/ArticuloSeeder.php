a26d888c2b9faac234106da9897b4f00df39eb37
marianoflowers <mariano.alfredo.flores@gmail.com>
2021-09-22 21:10:12 -0300

    [Update] Artículo Seeder

--- database/seeders/ArticuloSeeder.php
+++ database/seeders/ArticuloSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class ArticuloSeeder extends Seeder
 {
@@ -13,12 +12,23 @@ class ArticuloSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        DB::table('articulos')->insert([
-            'descripcion' => 'Lararopas Wirphool',
-            'precio' => 50000,
-            'stock' => 100,
-        ]);
+        $arrayArticulos = array(
+            array('descripcion' => 'Lararopas Wirphool', 'precio' => 50000, 'stock' => 100),
+            array('descripcion' => 'Lavaropas Drean', 'precio' => 55000, 'stock' => 15),
+            array('descripcion' => 'Cocina Volcán', 'precio' => 25000, 'stock' => 8),
+            array('descripcion' => 'Cafetera Sensei', 'precio' => 8000, 'stock' => 25),
+        );
+
+        foreach ($arrayArticulos as $articulo)
+        {
+            DB::table('articulos')->insert([
+                'descripcion' => $articulo['descripcion'],
+                'precio' => $articulo['precio'],
+                'stock' => $articulo['stock'],
+            ]);
+        }
     }
 }
